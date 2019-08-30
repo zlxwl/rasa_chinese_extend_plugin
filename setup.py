129457535c15f61cc95a708e27c68e0286981d2c
Zhong Lei <BG293198@800best.net>
2019-08-30 19:21:43 +0800

    'update'

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
 import setuptools
 
-with open("RE")
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="rasa_chinese_plugin",
+    version="1.0",
+    author="ZhongLei",
+    author_email="625015751@qq.com",
+    decciption="rasa中文拓展包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zlxwl/rasa_chinese_extend_plugin.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT Liense",
+        "Operating System :: OS Independent",
+    ],
+)
